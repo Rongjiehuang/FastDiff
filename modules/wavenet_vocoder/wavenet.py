1f35b1e0e7559f2dae5c32c43d5f593700a08333
Rongjiehuang <935012782@qq.com>
2022-05-20 16:08:15 +0800

    init

---
+++ modules/wavenet_vocoder/wavenet.py
@@ -0,0 +1,485 @@
+# coding: utf-8
+from __future__ import with_statement, print_function, absolute_import
+
+import math
+import numpy as np
+
+import torch
+from torch import nn
+from torch.nn import functional as F
+from tqdm import tqdm
+from utils.hparams import hparams
+from .modules import Embedding
+
+from .modules import Conv1d1x1, ResidualConv1dGLU, ConvTranspose2d
+from .mixture import sample_from_discretized_mix_logistic
+from .mixture import sample_from_mix_gaussian
+from . import upsample
+
+
+def _expand_global_features(B, T, g, bct=True):
+    """Expand global conditioning features to all time steps
+
+    Args:
+        B (int): Batch size.
+        T (int): Time length.
+        g (Tensor): Global features, (B x C) or (B x C x 1).
+        bct (bool) : returns (B x C x T) if True, otherwise (B x T x C)
+
+    Returns:
+        Tensor: B x C x T or B x T x C or None
+    """
+    if g is None:
+        return None
+    g = g.unsqueeze(-1) if g.dim() == 2 else g
+    if bct:
+        g_bct = g.expand(B, -1, T)
+        return g_bct.contiguous()
+    else:
+        g_btc = g.expand(B, -1, T).transpose(1, 2)
+        return g_btc.contiguous()
+
+
+def receptive_field_size(total_layers, num_cycles, kernel_size,
+                         dilation=lambda x: 2 ** x):
+    """Compute receptive field size
+
+    Args:
+        total_layers (int): total layers
+        num_cycles (int): cycles
+        kernel_size (int): kernel size
+        dilation (lambda): lambda to compute dilation factor. ``lambda x : 1``
+          to disable dilated convolution.
+
+    Returns:
+        int: receptive field size in sample
+
+    """
+    assert total_layers % num_cycles == 0
+    layers_per_cycle = total_layers // num_cycles
+    dilations = [dilation(i % layers_per_cycle) for i in range(total_layers)]
+    return (kernel_size - 1) * sum(dilations) + 1
+
+
+class WaveNet(nn.Module):
+    """The WaveNet models that supports local and global conditioning.
+
+    Args:
+        out_channels (int): Output channels. If input_type is mu-law quantized
+          one-hot vecror. this must equal to the quantize channels. Other wise
+          num_mixtures x 3 (pi, mu, log_scale).
+        layers (int): Number of total layers
+        stacks (int): Number of dilation cycles
+        residual_channels (int): Residual input / output channels
+        gate_channels (int): Gated activation channels.
+        skip_out_channels (int): Skip connection channels.
+        kernel_size (int): Kernel size of convolution layers.
+        dropout (float): Dropout probability.
+        cin_channels (int): Local conditioning channels. If negative value is
+          set, local conditioning is disabled.
+        gin_channels (int): Global conditioning channels. If negative value is
+          set, global conditioning is disabled.
+        n_speakers (int): Number of speakers. Used only if global conditioning
+          is enabled.
+        upsample_conditional_features (bool): Whether upsampling local
+          conditioning features by transposed convolution layers or not.
+        upsample_scales (list): List of upsample scale.
+          ``np.prod(upsample_scales)`` must equal to hop size. Used only if
+          upsample_conditional_features is enabled.
+        freq_axis_kernel_size (int): Freq-axis kernel_size for transposed
+          convolution layers for upsampling. If you only care about time-axis
+          upsampling, set this to 1.
+        scalar_input (Bool): If True, scalar input ([-1, 1]) is expected, otherwise
+          quantized one-hot vector is expected.
+        use_speaker_embedding (Bool): Use speaker embedding or Not. Set to False
+          if you want to disable embedding layer and use external features
+          directly.
+    """
+
+    def __init__(self, out_channels=256, layers=20, stacks=2,
+                 residual_channels=512,
+                 gate_channels=512,
+                 skip_out_channels=512,
+                 kernel_size=3, dropout=1 - 0.95,
+                 cin_channels=-1, gin_channels=-1, n_speakers=None,
+                 upsample_conditional_features=False,
+                 upsample_net="ConvInUpsampleNetwork",
+                 upsample_params={"upsample_scales": [4, 4, 4, 4]},
+                 scalar_input=False,
+                 use_speaker_embedding=False,
+                 output_distribution="Logistic",
+                 cin_pad=0,
+                 use_pitch_embed=False
+                 ):
+        super(WaveNet, self).__init__()
+        self.scalar_input = scalar_input
+        self.out_channels = out_channels
+        self.cin_channels = cin_channels
+        self.output_distribution = output_distribution
+        assert layers % stacks == 0
+        layers_per_stack = layers // stacks
+        if scalar_input:
+            self.first_conv = Conv1d1x1(1, residual_channels)
+        else:
+            self.first_conv = Conv1d1x1(out_channels, residual_channels)
+
+        self.conv_layers = nn.ModuleList()
+        for layer in range(layers):
+            dilation = 2 ** (layer % layers_per_stack)
+            conv = ResidualConv1dGLU(
+                residual_channels, gate_channels,
+                kernel_size=kernel_size,
+                skip_out_channels=skip_out_channels,
+                bias=True,  # magenda uses bias, but musyoku doesn't
+                dilation=dilation, dropout=dropout,
+                cin_channels=cin_channels,
+                gin_channels=gin_channels)
+            self.conv_layers.append(conv)
+        self.last_conv_layers = nn.ModuleList([
+            nn.ReLU(inplace=True),
+            Conv1d1x1(skip_out_channels, skip_out_channels),
+            nn.ReLU(inplace=True),
+            Conv1d1x1(skip_out_channels, out_channels),
+        ])
+
+        if gin_channels > 0 and use_speaker_embedding:
+            assert n_speakers is not None
+            self.embed_speakers = Embedding(
+                n_speakers, gin_channels, padding_idx=None, std=0.1)
+        else:
+            self.embed_speakers = None
+
+        # Upsample conv net
+        if upsample_conditional_features:
+            self.upsample_net = getattr(upsample, upsample_net)(**upsample_params)
+        else:
+            self.upsample_net = None
+
+        self.receptive_field = receptive_field_size(layers, stacks, kernel_size)
+
+        self.use_pitch_embed = use_pitch_embed
+        if use_pitch_embed:
+            self.pitch_embed = nn.Embedding(300, cin_channels, 0)
+            self.c_proj = nn.Linear(2 * cin_channels, cin_channels)
+
+    def has_speaker_embedding(self):
+        return self.embed_speakers is not None
+
+    def local_conditioning_enabled(self):
+        return self.cin_channels > 0
+
+    def forward(self, x, c=None, g=None, softmax=False, p=None):
+        """Forward step
+
+        Args:
+            x (Tensor): One-hot encoded audio signal, shape (B x C x T)
+            c (Tensor): Local conditioning features,
+              shape (B x cin_channels x T)
+            g (Tensor): Global conditioning features,
+              shape (B x gin_channels x 1) or speaker Ids of shape (B x 1).
+              Note that ``self.use_speaker_embedding`` must be False when you
+              want to disable embedding layer and use external features
+              directly (e.g., one-hot vector).
+              Also type of input tensor must be FloatTensor, not LongTensor
+              in case of ``self.use_speaker_embedding`` equals False.
+            softmax (bool): Whether applies softmax or not.
+
+        Returns:
+            Tensor: output, shape B x out_channels x T
+        """
+        B, _, T = x.size()
+
+        if g is not None:
+            if self.embed_speakers is not None:
+                # (B x 1) -> (B x 1 x gin_channels)
+                g = self.embed_speakers(g.view(B, -1))
+                # (B x gin_channels x 1)
+                g = g.transpose(1, 2)
+                assert g.dim() == 3
+        # Expand global conditioning features to all time steps
+        g_bct = _expand_global_features(B, T, g, bct=True)
+
+        if c is not None and self.upsample_net is not None:
+            if self.use_pitch_embed:
+                c = self.c_proj(torch.cat([
+                    c.transpose(1, 2), self.pitch_embed(p)], -1)).transpose(1, 2)
+            c = self.upsample_net(c)
+            assert c.size(-1) == x.size(-1)
+
+        # Feed data to network
+        x = self.first_conv(x)
+        skips = 0
+        for f in self.conv_layers:
+            x, h = f(x, c, g_bct)
+            skips += h
+        skips *= math.sqrt(1.0 / len(self.conv_layers))
+
+        x = skips
+        for f in self.last_conv_layers:
+            x = f(x)
+
+        x = F.softmax(x, dim=1) if softmax else x
+
+        return x
+
+    def generate(self, c=None, p=None, fast=True):
+        B = c.shape[0]
+        if fast:
+            self.make_generation_fast_()
+
+        y_hat = self.incremental_forward(
+            c=c, p=p, tqdm=tqdm, softmax=True, quantize=True,
+            log_scale_min=hparams['log_scale_min'])
+
+        return y_hat.view(B, -1)
+
+    def incremental_forward(self, initial_input=None, c=None, p=None,
+                            g=None, tqdm=lambda x: x, softmax=True,
+                            quantize=True, log_scale_min=-50.0,
+                            batched=True, target=12800, overlap=512
+                            ):
+        """Incremental forward step
+
+        Due to linearized convolutions, inputs of shape (B x C x T) are reshaped
+        to (B x T x C) internally and fed to the network for each time step.
+        Input of each time step will be of shape (B x 1 x C).
+
+        Args:
+            initial_input (Tensor): Initial decoder input, (B x C x 1)
+            c (Tensor): Local conditioning features, shape (B x C' x T)
+            g (Tensor): Global conditioning features, shape (B x C'' or B x C''x 1)
+            T (int): Number of time steps to generate.
+            test_inputs (Tensor): Teacher forcing inputs (for debugging)
+            tqdm (lamda) : tqdm
+            softmax (bool) : Whether applies softmax or not
+            quantize (bool): Whether quantize softmax output before feeding the
+              network output to input for the next time step. TODO: rename
+            log_scale_min (float):  Log scale minimum value.
+
+        Returns:
+            Tensor: Generated one-hot encoded samples. B x C x T　
+              or scaler vector B x 1 x T
+        """
+        self.clear_buffer()
+
+        # Local conditioning
+        if c is not None:
+            if self.upsample_net is not None:
+                if self.use_pitch_embed:
+                    c = self.c_proj(torch.cat([
+                        c.transpose(1, 2), self.pitch_embed(p)], -1)).transpose(1, 2)
+                c = self.upsample_net(c)
+
+            if batched:
+                assert c.shape[0] == 1
+                c = self.fold_with_overlap(c.transpose(1, 2), target, overlap).transpose(1, 2)
+            c = c.transpose(1, 2).contiguous()
+        B = c.shape[0]
+
+        outputs = []
+        if initial_input is None:
+            if self.scalar_input:
+                initial_input = torch.zeros(B, 1, 1)
+            else:
+                initial_input = torch.zeros(B, 1, self.out_channels)
+                initial_input[:, :, 127] = 1  # TODO: is this ok?
+            # https://github.com/pytorch/pytorch/issues/584#issuecomment-275169567
+            if next(self.parameters()).is_cuda:
+                initial_input = initial_input.cuda()
+        else:
+            if initial_input.size(1) == self.out_channels:
+                initial_input = initial_input.transpose(1, 2).contiguous()
+
+        current_input = initial_input
+        for t in tqdm(range(c.shape[1])):
+            if t > 0:
+                current_input = outputs[-1]
+
+            # Conditioning features for single time step
+            ct = None if c is None else c[:, t, :].unsqueeze(1)
+            gt = None
+
+            x = current_input
+            x = self.first_conv.incremental_forward(x)
+            skips = 0
+            for f in self.conv_layers:
+                x, h = f.incremental_forward(x, ct, gt)
+                skips += h
+            skips *= math.sqrt(1.0 / len(self.conv_layers))
+            x = skips
+            for f in self.last_conv_layers:
+                try:
+                    x = f.incremental_forward(x)
+                except AttributeError:
+                    x = f(x)
+
+            # Generate next input by sampling
+            if self.scalar_input:
+                if self.output_distribution == "Logistic":
+                    x = sample_from_discretized_mix_logistic(
+                        x.view(B, -1, 1), log_scale_min=log_scale_min)
+                elif self.output_distribution == "Normal":
+                    x = sample_from_mix_gaussian(
+                        x.view(B, -1, 1), log_scale_min=log_scale_min)
+                else:
+                    assert False
+            else:
+                x = F.softmax(x.view(B, -1), dim=1) if softmax else x.view(B, -1)
+                if quantize:
+                    dist = torch.distributions.OneHotCategorical(x)
+                    x = dist.sample()
+            outputs += [x.data]
+        # T x B x C
+        outputs = torch.stack(outputs)
+        # B x C x T
+        outputs = outputs.transpose(0, 1).transpose(1, 2).contiguous()
+        if batched:
+            outputs = self.xfade_and_unfold(outputs[:, 0], target, overlap)[None, None, ...]
+
+        self.clear_buffer()
+        return outputs
+
+    def clear_buffer(self):
+        self.first_conv.clear_buffer()
+        for f in self.conv_layers:
+            f.clear_buffer()
+        for f in self.last_conv_layers:
+            try:
+                f.clear_buffer()
+            except AttributeError:
+                pass
+
+    def make_generation_fast_(self):
+        def remove_weight_norm(m):
+            try:
+                nn.utils.remove_weight_norm(m)
+            except ValueError:  # this module didn't have weight norm
+                return
+
+        self.apply(remove_weight_norm)
+
+    def fold_with_overlap(self, x, target, overlap):
+
+        ''' Fold the tensor with overlap for quick batched inference.
+            Overlap will be used for crossfading in xfade_and_unfold()
+
+        Args:
+            x (tensor)    : Upsampled conditioning features.
+                            shape=(1, timesteps, features)
+            target (int)  : Target timesteps for each index of batch
+            overlap (int) : Timesteps for both xfade and rnn warmup
+
+        Return:
+            (tensor) : shape=(num_folds, target + 2 * overlap, features)
+
+        Details:
+            x = [[h1, h2, ... hn]]
+
+            Where each h is a vector of conditioning features
+
+            Eg: target=2, overlap=1 with x.size(1)=10
+
+            folded = [[h1, h2, h3, h4],
+                      [h4, h5, h6, h7],
+                      [h7, h8, h9, h10]]
+        '''
+
+        _, total_len, features = x.size()
+
+        # Calculate variables needed
+        num_folds = (total_len - overlap) // (target + overlap)
+        extended_len = num_folds * (overlap + target) + overlap
+        remaining = total_len - extended_len
+
+        # Pad if some time steps poking out
+        if remaining != 0:
+            num_folds += 1
+            padding = target + 2 * overlap - remaining
+            x = self.pad_tensor(x, padding, side='after')
+
+        folded = torch.zeros(num_folds, target + 2 * overlap, features, device=x.device)
+
+        # Get the values for the folded tensor
+        for i in range(num_folds):
+            start = i * (target + overlap)
+            end = start + target + 2 * overlap
+            folded[i] = x[:, start:end, :]
+
+        return folded
+
+    def xfade_and_unfold(self, y, target, overlap):
+
+        ''' Applies a crossfade and unfolds into a 1d array.
+
+        Args:
+            y (ndarry)    : Batched sequences of audio samples
+                            shape=(num_folds, target + 2 * overlap)
+                            dtype=np.float64
+            overlap (int) : Timesteps for both xfade and rnn warmup
+
+        Return:
+            (ndarry) : audio samples in a 1d array
+                       shape=(total_len)
+                       dtype=np.float64
+
+        Details:
+            y = [[seq1],
+                 [seq2],
+                 [seq3]]
+
+            Apply a gain envelope at both ends of the sequences
+
+            y = [[seq1_in, seq1_target, seq1_out],
+                 [seq2_in, seq2_target, seq2_out],
+                 [seq3_in, seq3_target, seq3_out]]
+
+            Stagger and add up the groups of samples:
+
+            [seq1_in, seq1_target, (seq1_out + seq2_in), seq2_target, ...]
+
+        '''
+
+        num_folds, length = y.shape
+        target = length - 2 * overlap
+        total_len = num_folds * (target + overlap) + overlap
+
+        # Need some silence for the rnn warmup
+        silence_len = overlap // 2
+        fade_len = overlap - silence_len
+        silence = y.new_zeros([silence_len])
+        linear = y.new_ones([silence_len])
+
+        # Equal power crossfade
+        t = torch.linspace(-1, 1, fade_len).to(y.device)
+        fade_in = torch.sqrt(0.5 * (1 + t))
+        fade_out = torch.sqrt(0.5 * (1 - t))
+
+        # Concat the silence to the fades
+        fade_in = torch.cat([silence, fade_in])
+        fade_out = torch.cat([linear, fade_out])
+
+        # Apply the gain to the overlap samples
+        y[:, :overlap] *= fade_in
+        y[:, -overlap:] *= fade_out
+
+        unfolded = y.new_zeros([total_len])
+
+        # Loop to add up all the samples
+        for i in range(num_folds):
+            start = i * (target + overlap)
+            end = start + target + 2 * overlap
+            unfolded[start:end] += y[i]
+
+        return unfolded
+
+    def pad_tensor(self, x, pad, side='both'):
+        # NB - this is just a quick method i need right now
+        # i.e., it won't generalise to other shapes/dims
+        b, t, c = x.size()
+        total = t + 2 * pad if side == 'both' else t + pad
+        padded = torch.zeros(b, total, c, device=x.device)
+        if side == 'before' or side == 'both':
+            padded[:, pad:pad + t, :] = x
+        elif side == 'after':
+            padded[:, :t, :] = x
+        return padded
