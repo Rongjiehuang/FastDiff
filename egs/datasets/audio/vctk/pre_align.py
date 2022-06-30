69bb8f6c07186a0b802439a53bf273261ff0fa1e
Rongjiehuang <935012782@qq.com>
2022-06-30 13:21:41 +0800

    update

--- egs/datasets/audio/vctk/pre_align.py
+++ egs/datasets/audio/vctk/pre_align.py
@@ -10,12 +10,9 @@ class VCTKPreAlign(VocoderPreAlign):
         for wav_fn in wav_fns:
             item_name = os.path.basename(wav_fn)[:-4]
             spk = item_name.split("_")[0]
-            txt_fn = wav_fn.split("/")
-            txt_fn[-1] = f'{item_name}.txt'
-            txt_fn[-3] = f'txt'
-            txt_fn = "/".join(txt_fn)
-            if os.path.exists(txt_fn) and os.path.exists(wav_fn):
-                yield item_name, wav_fn, (self.load_txt, txt_fn), spk
+            txt = "Not Needed"
+            if os.path.exists(wav_fn):
+                yield item_name, wav_fn, txt, spk
 
 
 if __name__ == "__main__":
